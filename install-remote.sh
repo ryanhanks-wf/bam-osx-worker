3a6b1d29c4b898f67b157c9ffb94be3e2b91a454
Farmer <rickfarmer@users.noreply.github.com>
2015-03-03 23:27:41 -0600

    remote var

--- install-remote.sh
+++ install-remote.sh
@@ -2,8 +2,8 @@
 
 # Run using:
 #
-# \curl -Ls https://raw.githubusercontent.com/pivotalservices/sprout-wrap-pivotal/master/install-remote.sh | bash
+# \curl -Ls https://raw.githubusercontent.com/pivotalservices/sprout-wrap-pivotal/master/install-remote.sh | bash -c "$SCRIPT"
 
 # To enable pausing within the script it is necessary to load then execute the install.sh
-SCRIPT=$(curl -s https://raw.githubusercontent.com/pivotalservices/sprout-wrap-pivotal/master/pause.sh)
-bash -c "$SCRIPT"
+SCRIPT=$(curl -s https://raw.githubusercontent.com/pivotalservices/sprout-wrap-pivotal/master/install.sh)
+# bash -c "$SCRIPT"
